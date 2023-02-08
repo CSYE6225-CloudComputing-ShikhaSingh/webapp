eb029c853cf4a4101644ad9182a95027a75001ab
singh.shi <singh.shikha@northeastern.edu>
2023-02-08 17:51:42 -0500

    Updating app.js

--- app.js
+++ app.js
@@ -1,37 +1,80 @@
 const { truncateSync } = require('fs');
-const {sequelize,User} = require('./models')
+var queryType = require('query-types');
+
+const {sequelize} = require('./models')
 const express= require('express');
-const bcrypt= require('bcrypt');
 const postRouter= require('./Controller/create');
 const getRouter= require('./Controller/findUser');
 const updateRouter= require('./Controller/update');
+const updateProduct= require('./Controller/updateProduct');
+
+const deleteProductRouter= require('./Controller/deleteProduct');
 const health= require('./Controller/health');
+const addProduct= require('./Controller/addProduct');
+const getProduct= require('./Controller/getProduct');
+
+require('dotenv').config();
+const port= process.env.PORT || 3000
 
 
 const app= express();
 app.use(express.json());
+app.use(queryType.middleware())
+
 var validator = require("email-validator");
 var passwordValidator = require('password-validator');
 var schema = new passwordValidator();
 var auth= require('./auth/auth')
 
 //Unauthenticated Post Rest api to create users
-app.use('/user',postRouter);
-
+app.use(postRouter);
 app.use(getRouter);
+app.use(getProduct);
+
 
 // Authenticated API - put request to update user details
 app.use(updateRouter);
-
+app.use(updateProduct);
+app.use(deleteProductRouter);
 app.use(health);
+app.use(addProduct);
 
-app.listen({port:3030},async()=>{
-    console.log("Server is running at port 3030");
-    await sequelize.sync();
-    await sequelize.authenticate()
-    console.log("Database is connected")
+const connectDb= async()=>{
+    console.log("checking the database connection")
+    try{
+      await sequelize.authenticate();  
+      console.log("Database connection established")
+    }
+    catch(e){
+     console.log("Database connection failed")
+     process.exit(1);
+    }
+}
+//making a self executed function
+(async()=>{
+   await connectDb();
+   console.log(`Attempting to run server on ${port}`)
+   app.listen(port,()=>{
+    console.log(`Server is running at ${port}`);
+
+   })
+
+})()
+// app.listen({port:3030},async()=>{
+//     console.log("Server is running at port 3030");
+//     sequelize
+//     .authenticate()
+//     .then(() => {
+//         console.log('Connection to the database has been established successfully.');
+//     })
+//     .catch(error => {
+//         console.error(error);
+//     });
+
+// sequelize.sync();
+
     
-})
+// })
 
 module.exports = app
 
