b35760bcf4b70a1c30c4ef6ab317b7cde7d55dfa
singh.shi <singh.shikha@northeastern.edu>
2023-02-09 09:26:25 -0500

    migration file

---
+++ migrations/20230206231052-create-products.js
@@ -0,0 +1,49 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, DataTypes) {
+    await queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      name: {
+        type: DataTypes.STRING
+      },
+      description: {
+        type: DataTypes.STRING
+      },
+      manufacturer: {
+        type: DataTypes.STRING
+      },
+      sku: {
+        type: DataTypes.STRING
+      },
+      quantity: {
+        type: DataTypes.INTEGER
+      },
+      owner_user_id:{
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references:{
+          model:'Users',   //here it requires table name and not model name
+          key:'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      
+    });
+  },
+  async down(queryInterface, DataTypes) {
+    await queryInterface.dropTable('products');
+  }
+};
