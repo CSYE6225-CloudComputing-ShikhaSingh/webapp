88dfa250e4c634e2fb9e83f8fb038d38f12c080f
singh.shi <singh.shikha@northeastern.edu>
2023-02-15 23:14:22 -0500

    Final commit

--- models/user.js
+++ models/user.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsTo(models.Product,{foreignKey:'owner_user_id'});
     }
   }
   User.init({
