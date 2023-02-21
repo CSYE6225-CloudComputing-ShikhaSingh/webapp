e99da94c39cb933e44feaf964fe32b491db4b5d1
singh.shi <singh.shikha@northeastern.edu>
2023-02-21 14:03:30 -0500

    changed node version in config.js

--- config/config.js
+++ config/config.js
@@ -4,12 +4,12 @@ require('dotenv').config()
 module.exports=
 {
   "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
+    "username": "postgres",
+    "password": "pgadmin",
+    "database": "cloudpostgres",
+    "host": "127.0.0.0/1",
     "dialect": 'postgres',
-    "port": process.env.DB_PORT
+    "port": "5432"
   },
   "test": {
     "username": "root",
