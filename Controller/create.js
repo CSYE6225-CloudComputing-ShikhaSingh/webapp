41aafaa2595eab380ed900a02e41664d0a8a820b
Shikha Singh <114118576+shikha-commits@users.noreply.github.com>
2023-04-19 14:32:25 -0400

    Update create.js

--- Controller/create.js
+++ Controller/create.js
@@ -12,7 +12,7 @@ const client = new Client("localhost", 8125);
 
 
 //Unauthenticated Post Rest api to create users
-router.post('/v1/user',async(req,res)=>{
+router.post('/v2/user',async(req,res)=>{
 
     logger.info('POST USER API called - User creation process started');
     
